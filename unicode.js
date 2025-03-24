31daeeada9f15076a5ec63eca7f4c4dd56ffdb7a
stephaniev <veile.steph@gmail.com>
2025-03-24 01:02:00 -0500

    Completed activities

--- unicode.js
+++ unicode.js
@@ -16,6 +16,19 @@ let inputString = "Hi";
 Expected Output:
 sumCodePoints: 177 // 'H' = 72, 'i' = 105, 72 + 105 = 177
 
+*/
+
+console.log("Practice Problem #1 Exercise 1:");
+
+let inputString = ("Hi");
+let codePoint01 = inputString.charCodeAt(0);
+let codePoint02 = inputString.charCodeAt(1);
+console.log("H = " + codePoint01 + " and i = " + codePoint02);
+let sumCodePoints = codePoint01 + codePoint02;
+console.log(sumCodePoints);
+
+/*
+
 Exercise 2: Generate a String from Two Code Points
 Objective: Take two numeric Unicode code points,
 convert them to characters using String.fromCharCode,
@@ -31,6 +44,17 @@ let codePoint2 = 66;
 
 Expected Output:
 combinedString: "AB" // 65 = 'A', 66 = 'B', combined = "AB"
+
+*/
+
+console.log(`\nPractice Problem #1 Exercise 2:`);
+
+let codePoint1 = 65;
+let codePoint2 = 66;
+let combinedString = String.fromCharCode(codePoint1) + String.fromCharCode(codePoint2);
+console.log(combinedString);
+
+/*
 
 Exercise 3: Find the Character Difference
 Objective: Extract the Unicode code points of two given characters from a string
@@ -48,7 +72,19 @@ let index2 = 2;
 Expected Output:
 codePointDifference: 16 // 'C' = 67, 't' = 116, |67 - 116| = 16
 
+*/
 
+console.log(`\nPractice Problem #1 Exercise 1:`);
+
+let inputString3 = "Cat";
+let index1 = 0;
+let index2 = 2;
+
+let codePointDifference = Math.abs(inputString3.charCodeAt(index1) - inputString3.charCodeAt(index2));
+
+console.log(codePointDifference);
+
+/*
 
 Practice Problem #2
 
@@ -86,15 +122,19 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0);
+let thirdCodePoint = inputString1.charCodeAt(2);
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72) + String.fromCharCode(101) + String.fromCharCode(108) + String.fromCharCode(108);
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let codeTask31 = inputString.charCodeAt(0);
+console.log(codeTask31);
+let codeTask32 = inputString.charCodeAt(5);
+console.log(codeTask32);
+let swappedString = String.fromCharCode(Number(codeTask32)) + "aunc" + String.fromCharCode(Number(codeTask31));
 
 // Log all results
 console.log({
